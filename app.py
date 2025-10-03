c62d897b001da2345b522754511491e19e7707bf
juddson <105756075+juddson@users.noreply.github.com>
2025-10-03 02:47:53 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Hello, team!"
+
+if __name__ == "__main__":
+    # Codespaces forwards ports automatically
+    app.run(host="0.0.0.0", port=5000, debug=True)
