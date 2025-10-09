269eb4e87b69f1745d019f4c7f97c2242ebc5fac
juddson <105756075+juddson@users.noreply.github.com>
2025-10-09 05:44:50 +0000

    added temp favicon, about/join pages, corrected styling for current nav buttons

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", active_page="home")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", active_page="about")
+
+@app.route("/join")
+def join():
+    return render_template("join.html", active_page="join")
 
 if __name__ == "__main__":
     # Codespaces forwards ports automatically
