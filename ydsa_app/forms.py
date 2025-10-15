b5724757474f0e273c5333f294bab742d9664d9b
juddson <105756075+juddson@users.noreply.github.com>
2025-10-15 05:42:08 +0000

    finished form for email and name submission and corrected styling

--- ydsa_app/forms.py
+++ ydsa_app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, HiddenField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, ValidationError
 
 class JoinForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired(), Length(max=120)])
@@ -9,3 +9,7 @@ class JoinForm(FlaskForm):
     # Honeypot (anti-bot): leave empty in UI
     website = HiddenField()  # bots often fill everything
     submit = SubmitField("Join")
+
+    def validate_email(self, field):
+        if not field.data.lower().endswith("@uab.edu"):
+            raise ValidationError("Please use your @uab.edu email address.")
